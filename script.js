76742b456c1aaf0a32f3780760d3f51f8f900f39
itssarah <43419795+itssarah@users.noreply.github.com>
2021-11-11 12:24:51 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var svg = d3.select("body").append("svg")
+                                    .attr("width", 600)
+                                    .attr("height", 400)
+                                    .style('background-color',"#ffeb3b");
