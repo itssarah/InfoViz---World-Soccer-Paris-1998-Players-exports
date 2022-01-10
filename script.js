8209a3f3d991203931a951add0c5c4ccd61bfbe7
itssarah <43419795+itssarah@users.noreply.github.com>
2022-01-10 08:20:49 +0100

    exercice 1 - cercle & line

--- script.js
+++
@@ -1,4 +0,0 @@
-var svg = d3.select("body").append("svg")
-                                    .attr("width", 600)
-                                    .attr("height", 400)
-                                    .style('background-color',"#ffeb3b");
